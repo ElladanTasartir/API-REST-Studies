ee5d4809023d031fa54905f15753f06318423a36
Erick Malta <erickmalta100@gmail.com>
2020-06-01 17:09:30 -0300

    Adicionado um pouco mais de conteúudo

--- app.js
+++ app.js
@@ -1,10 +1,60 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
 
-const rotaProdutos = require('./routes/produtos');
-const rotaPedidos = require('./routes/pedidos');
+//CORS = Cross Origin Resource Sharing
+//Funcionalidade que permite que um site acesse outro site dependendo de algumas restrições
 
-app.use('/produtos', rotaProdutos);
-app.use('/pedidos', rotaPedidos);
+const rotaProdutos = require("./routes/produtos");
+const rotaPedidos = require("./routes/pedidos");
+
+app.use(morgan("dev"));
+//O middleware morgan irá passar por nossas requisições e logar
+//O método http usado, a rota, o status code, o tempo de execução, entre outros fatores
+app.use(bodyParser.urlencoded({ extended: false }));
+//significa que apenas aceitaremos dados simples, ou seja, não podemos colocar nested objects dentro
+//do corpo da requisiçã por exemplo, quando o formato é o form url encoded
+app.use(bodyParser.json());
+//permite com que o body faça o parsing do json
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  //Dando permissão de acesso a * (todos) os endereços de request
+  //Se fosse um servidor em produção, poderia colocar o endreço que deseja atender àquela API, como
+  //"Acess-Control-Allow-Origin", "http://foo.bar.com"
+  res.header(
+    "Acess-Control-Allow-Header",
+    "Origin, X-Requested-With, Accept, Authorization, Content-Type"
+  ); //Quais os headers que a nossa API vai aceitar receber
+
+  if (req.method === "OPTIONS") {
+    //Se o método usado for o OPTIONS, passaremos
+    //como permitidos esses métodos http:
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).send({});
+  }
+
+  next();
+});
+
+app.use("/produtos", rotaProdutos);
+app.use("/pedidos", rotaPedidos);
+
+//Tratamento para quando não encontrar nenhuma rota
+app.use((req, res, next) => {
+  const erro = new Error("Não encontrado");
+  erro.status = 404;
+  next(erro); //Vai passar o erro para o próximo middleware
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  return res.send({
+    erro: {
+      message: error.message, //Vai pegar a mensagem do erro enviada no Error() e renderizar na tela
+    },
+  });
+});
 
 module.exports = app;
--- routes/pedidos.js
+++ routes/pedidos.js
@@ -1,45 +1,51 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    res.status(200).send({
-        mensagem: 'Usando o get dentro da rota de pedidos'
-    });
+router.get("/", (req, res, next) => {
+  res.status(200).send({
+    mensagem: "Usando o get dentro da rota de pedidos",
+  });
 });
 
-router.post('/', (req, res, next) => {
-    res.status(201).send({
-        mensagem: 'Usando o post dentro da rota de pedidos'
-    });
+router.post("/", (req, res, next) => {
+  const pedido = {
+    id_produto: req.body.id_produto,
+    quantidade: req.body.quantidade,
+  };
+
+  res.status(201).send({
+    mensagem: "Usando o post dentro da rota de pedidos",
+    pedidoCriado: pedido,
+  });
 });
 
-router.get('/:id_pedido', (req, res, next) => {
-    const id = req.params.id_pedido;
+router.get("/:id_pedido", (req, res, next) => {
+  const id = req.params.id_pedido;
 
-    if (id === 'especial') {
-        return res.status(200).send({
-            mensagem: 'Descobriu o id especial',
-            id
-        });
-    }
-
-    res.status(200).send({
-        mensagem: 'Usando o GET de um pedido exclusivo',
-        id
+  if (id === "especial") {
+    return res.status(200).send({
+      mensagem: "Descobriu o id especial",
+      id,
     });
+  }
+
+  res.status(200).send({
+    mensagem: "Usando o GET de um pedido exclusivo",
+    id,
+  });
 });
 
-router.patch('/', (req, res, next) => { 
-    res.status(201).send({
-        mensagem: 'Usando o PATCH dentro da rota de pedidos'
-    });
+router.patch("/", (req, res, next) => {
+  res.status(201).send({
+    mensagem: "Usando o PATCH dentro da rota de pedidos",
+  });
 });
 
-router.delete('/:id', (req,res, next) => {
-    res.status(201).send({
-        mensagem: 'Usando o DELETE dentro da rota de pedidos',
-        id: req.params.id
-    });
+router.delete("/:id", (req, res, next) => {
+  res.status(201).send({
+    mensagem: "Usando o DELETE dentro da rota de pedidos",
+    id: req.params.id,
+  });
 });
 
 module.exports = router;
